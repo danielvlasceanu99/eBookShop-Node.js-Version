2b16ee1f055e245d4aef664f1b0989246965ac37
Daniel <danielvlasceanu99@gmail.com>
2021-08-10 15:18:59 +0300

    Added get(All)Book, update and delete controllers

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { Op } = require("sequelize");
 const BookDb = require("../models").Book;
 const AuthorDB = require("../models").Author;
 
@@ -92,6 +93,159 @@ const controller = {
         } else {
             res.status(400).send(errors);
         }
+    },
+
+    getAllBooks: async (req, res) => {
+        const queryParams = {
+            title: "%%",
+            genre: req.query.genre,
+        };
+        if (req.query.title) {
+            queryParams.title = "%" + req.query.title + "%";
+        }
+        try {
+            var books = await BookDb.findAll({
+                where: {
+                    title: {
+                        [Op.like]: queryParams.title,
+                    },
+                },
+                include: [AuthorDB],
+            });
+            if (queryParams.genre) {
+                books = books.filter((book) => book.genre === queryParams.genre);
+            }
+            res.status(201).send(books);
+        } catch {
+            res.status(500).send({ message: "Server error" });
+        }
+    },
+
+    getBook: async (req, res) => {
+        try {
+            const book = await BookDb.findByPk(req.params.id, {
+                include: [AuthorDB],
+            });
+            if (book) {
+                res.status(201).send(book);
+            } else {
+                res.status(404).send({ message: "Book not found" });
+            }
+        } catch {
+            res.status(500).send({ message: "Server error" });
+        }
+    },
+
+    updateBook: async (req, res) => {
+        const book = await BookDb.findByPk(req.params.id);
+        if (book) {
+            const errors = {};
+
+            if (!req.body.isbn) {
+                errors.isbn = "Missing ISBN";
+                console.log("Missing ISBN");
+            } else if (!req.body.isbn.match("^(?=(?:\\D*\\d){10}(?:(?:\\D*\\d){3})?$)[\\d-]+$")) {
+                errors.isbn = "Invalid ISBN";
+                console.log("Invalid ISBN");
+            } else {
+                const existing = await BookDb.findOne({
+                    where: { isbn: req.body.isbn },
+                });
+
+                if (existing && existing.id !== book.id) {
+                    errors.isbn = "ISBN already used";
+                    console.log("ISBN Already used");
+                }
+            }
+
+            if (!req.body.title) {
+                errors.title = "Missing title";
+                console.log("MIssing title");
+            } else {
+                req.body.title = req.body.title
+                    .split(" ")
+                    .map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase())
+                    .join(" ");
+            }
+
+            if (!req.body.description) {
+                errors.description = "Missing description";
+                console.log("Missing description");
+            }
+
+            if (!req.body.genre) {
+                errors.genre = "Missing genre";
+                console.log("Missing genre");
+            }
+
+            if (!req.body.price) {
+                errors.price = "Missing price";
+                console.log("Missing price");
+            }
+
+            if (!req.body.discount) {
+                book.discount = 0;
+            } else if (req.body.discount > 1 || req.body.discount < 0) {
+                errors.discount = "Invalid discount";
+                console.log("Invalid discount");
+            }
+
+            if (!req.body.imageName) {
+                errors.imageName = "Missing image name";
+                console.log("Missing image name");
+            }
+            if (!req.body.authorId) {
+                errors.authorId = "Missing author";
+                console.log("Missing author");
+            } else {
+                const author = await AuthorDB.findByPk(req.body.authorId);
+                if (!author) {
+                    errors.authorId = "Inexistent author";
+                    console.log("Inexistent author");
+                }
+            }
+
+            if (Object.keys(errors).length === 0) {
+                console.log("here");
+                await book
+                    .update({
+                        isbn: req.body.isbn,
+                        title: req.body.title,
+                        description: req.body.description,
+                        genre: req.body.genre,
+                        rating: 0,
+                        price: req.body.price,
+                        discount: req.body.discount,
+                        imageName: req.body.imageName,
+                        authorId: req.body.authorId,
+                    })
+                    .then(() => {
+                        res.status(201).send({ message: "Book updated" });
+                    })
+                    .catch(() => {
+                        res.status(500).send({ message: "Server error" });
+                    });
+            } else {
+                res.status(400).send(errors);
+            }
+        } else {
+            res.status(404).send({ message: "Book not found" });
+        }
+    },
+
+    deleteBook: async (req, res) => {
+        await BookDb.findByPk(req.params.id)
+            .then(async (product) => {
+                if (product) {
+                    await product.destroy();
+                    res.status(201).send({ message: "Book deleted" });
+                } else {
+                    res.status(404).send({ message: "Book not found" });
+                }
+            })
+            .catch(() => {
+                res.status(500).send({ message: "Server error" });
+            });
     },
 };
 
--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -4,5 +4,9 @@ const router = express.Router();
 const bookController = require("../controllers").bookController;
 
 router.post("/addBook", bookController.addBook);
+router.get("/getAllBooks", bookController.getAllBooks);
+router.get("/getBook/:id", bookController.getBook);
+router.put("/updateBook/:id", bookController.updateBook);
+router.delete("/deleteBook/:id", bookController.deleteBook);
 
 module.exports = router;
