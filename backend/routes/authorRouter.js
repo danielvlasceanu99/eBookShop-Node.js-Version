d08d684fcd087609d1a3e3590f827fd2c11ef035
Daniel <danielvlasceanu99@gmail.com>
2021-08-13 21:38:18 +0300

    Added get(all)Authors, updateAuthor and deleteAuthor plus bug fixes

--- backend/routes/authorRouter.js
+++ backend/routes/authorRouter.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const authorController = require("../controllers").authorController;
 
 router.post("/addAuthor", authorController.addAuthor);
+router.get("/getAllAuthors", authorController.getAllAuthors);
+router.get("/getAuthor/:id", authorController.getAuthor);
+router.put("/updateAuthor/:id", authorController.updateAuthor);
+router.delete("/deleteAuthor/:id", authorController.deleteAuthor);
 
 module.exports = router;
